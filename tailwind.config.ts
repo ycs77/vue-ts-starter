a91df555ccc7d8c2eda73a86d1b6e3cbb29e211f
Lucas Yang <yangchenshin77@gmail.com>
2023-04-02 21:37:09 +0800

    Upgrade to tailwindcss v3.3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
@@ -8,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config
