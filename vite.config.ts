1a181fbf59665e4057bfe928c66dd656006ba2c3
Lucas Yang <yangchenshin77@gmail.com>
2024-03-12 22:29:06 +0800

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import Vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
+import Pages from 'vite-plugin-pages'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
-import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
   plugins: [
