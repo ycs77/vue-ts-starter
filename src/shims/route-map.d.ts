32c067af4fd5251610d68f4e90f66d9a399ddbd6
Lucas Yang <yangchenshin77@gmail.com>
2026-06-29 10:26:32 +0800

    Bump dependencies & upgrade to pnpm v11

--- src/shims/route-map.d.ts
+++ src/shims/route-map.d.ts
@@ -14,11 +14,15 @@ import type {
   ParamValueZeroOrMore,
   ParamValueZeroOrOne,
 } from 'vue-router'
+import type {
+  _ExtractParamParserType,
+} from 'vue-router/experimental'
 
 declare module 'vue-router' {
   interface TypesConfig {
-    ParamParsers:
-      | never
+    _ParamParsers: {}
+    RouteNamedMap: import('vue-router/auto-routes').RouteNamedMap
+    _RouteFileInfoMap: import('vue-router/auto-routes')._RouteFileInfoMap
   }
 }
 
@@ -51,6 +55,8 @@ declare module 'vue-router/auto-routes' {
       routes:
         | '/'
       views:
+        | never
+      pathParamNames:
         | never
     }
   }
