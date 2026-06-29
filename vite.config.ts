020e3782992c436993cfaa474f7296f4a69633ab
Lucas Yang <yangchenshin77@gmail.com>
2026-06-29 10:19:57 +0800

    Remove unused auto-import and component plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'vue-router/vite'
 import TailwindCSS from '@tailwindcss/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
-import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig({
   plugins: [
@@ -15,24 +12,6 @@ export default defineConfig({
     }),
     Vue(),
     TailwindCSS(),
-    AutoImport({
-      imports: [
-        'vue',
-        {
-          'vue-router': ['useRoute', 'useRouter'],
-          '@vueuse/core': [],
-        },
-      ],
-      dirs: ['src/composables', 'src/utils'],
-      dts: 'src/shims/auto-imports.d.ts',
-      vueTemplate: true,
-    }),
-    Components({
-      resolvers: [
-        IconsResolver({ prefix: '' }),
-      ],
-      dts: 'src/shims/components.d.ts',
-    }),
     Icons(),
   ],
   resolve: {
